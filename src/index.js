957180f7b6db8ebb398fe4aabe3b8e87b14ddd4b
OOPS29 <priyanshusrivastava431343@gmail.com>
2026-09-05 01:37:57 +0530

    utils code discussion

--- src/index.js
+++ src/index.js
@@ -1,11 +1,23 @@
 import dns from "node:dns";
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 
 dns.setServers(["1.1.1.1", "8.8.8.8"]);
 dotenv.config({ path: "./.env" });
 
-connectDB();
+connectDB()
+.then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`Server is running on port ${process.env.PORT || 8000}`);
+    });
+})
+
+
+.catch((error) => {
+    console.error("Error connecting to MongoDB!!!", error);
+    process.exit(1);
+})
 
 
 
